69a199846ece9c54539105cd9a281d0cac1b6cb4
raphael.ferrao <raphael.ferrao@gmail.com>
2020-03-03 07:47:42 -0300

    Adicionando uma tabela de dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 import { AppComponent } from './app.component';
+import {TableModule} from 'primeng/table';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, InputTextModule, ButtonModule, TableModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
