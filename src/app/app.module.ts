9d0e6c327c1c1c772895eb6273afab928d6524e2
raphael.ferrao <raphael.ferrao@gmail.com>
2020-03-03 22:44:24 -0300

    criando componente de pesquisa de pessoas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import {TableModule} from 'primeng/table';
 import {TooltipModule} from 'primeng/tooltip';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
 
 
 @NgModule({
-  declarations: [AppComponent, LancamentosPesquisaComponent, NavbarComponent],
+  declarations: [AppComponent, LancamentosPesquisaComponent, NavbarComponent, PessoasPesquisaComponent],
   imports: [BrowserModule, InputTextModule, ButtonModule, TableModule, TooltipModule],
   providers: [],
   bootstrap: [AppComponent]
