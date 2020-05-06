04c55374656acfbcd0f36fc53c5a4e1fe057a20c
Raphael Ferrao <raphael.ferrao@thomsonreuters.com>
2020-05-06 18:16:31 -0300

    inicio do desenvolvimento da pagina de cadastro de pessoa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,22 +10,24 @@ import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { CalendarModule } from 'primeng/calendar';
-import {CurrencyMaskModule} from 'ng2-currency-mask';
+import { InputMaskModule } from 'primeng/inputmask';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
 
 import { AppComponent } from './app.component';
 import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
 
 
 @NgModule({
   declarations: [AppComponent, LancamentosPesquisaComponent, NavbarComponent,
-    PessoasPesquisaComponent, LancamentoCadastroComponent],
+    PessoasPesquisaComponent, LancamentoCadastroComponent, PessoaCadastroComponent],
   imports: [BrowserModule, BrowserAnimationsModule, InputTextModule,
     ButtonModule, TableModule, TooltipModule,
     InputTextareaModule, CalendarModule, SelectButtonModule,
-    DropdownModule, CurrencyMaskModule
+    DropdownModule, CurrencyMaskModule, InputMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
